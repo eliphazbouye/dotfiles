0bb9189ea0054397a54798e840b023fdb208d499
Eliphaz Bouye <eliphazbouye@gmail.com>
2023-11-25 14:34:08 +0000

    dotfiles

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  { 
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require('lspconfig')
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      lspconfig.tsserver.setup {
+        capabilities = capabilities,
+      }
+      lspconfig.pyright.setup {
+        capabilities = capabilities,
+      }
+      lspconfig.intelephense.setup {
+        capabilities = capabilities,
+      }
+      lspconfig.lua_ls.setup {
+        capabilities = capabilities,
+      }
+    end
+  },
+}
